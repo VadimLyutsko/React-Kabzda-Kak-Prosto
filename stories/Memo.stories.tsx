113b7abfaa79f7411f61c60382614c4d9a7abc69
vadim <vadim_lyutsko@mail.ru>
2022-12-10 21:48:08 +0300

    Add useCallback

--- stories/Memo.stories.tsx
+++ stories/Memo.stories.tsx
@@ -1,5 +1,5 @@
 // @ts-ignore
-import React, {useMemo, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 
 export default {
     title: 'Memo',
@@ -25,19 +25,56 @@ export const HelpsToReactMemo = () => {
     const [users, setUsers] = useState(['1111111', '2222222', '3333333', 'Vadim']);
 
     const newArray = useMemo(() => {
-        const newArray= users.filter(item => !item.toLowerCase().includes('2') );
-        return newArray
+        return  users.filter(item => !item.toLowerCase().includes('2'));
     }, [users]);
 
-    const addUser = ()=>{
-        setUsers([...users, 'Petro' + new  Date().getTime()])
-    }
+    const addUser = () => {
+        setUsers([...users, 'Petro' + new Date().getTime()]);
+    };
 
 
     return <>
-    <button onClick={()=>setCounter(counter+1)}>+</button>
-    <button onClick={()=>addUser()}>Add user</button>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        <button onClick={() => addUser()}>Add user</button>
         {counter}
         <Users users={newArray}/>
     </>;
 };
+
+//useCallBack
+
+type BooksSecretPropsType = {
+    addBook: () => void
+}
+const BooksSecret = (props: BooksSecretPropsType) => {
+    console.log('BooksSecret');
+    return <div>
+        <button onClick={() => props.addBook()}>Add book</button>
+    </div>;
+};
+
+const Book = React.memo(BooksSecret);
+
+export const LikeUseCallback = () => {
+    console.log('LikeUseCallback');
+    const [counter, setCounter] = useState(0);
+    const [books, setBooks] = useState(['aaaaaaaa', 'bbbbbbbb', '22d2d', 'Harry']);
+
+    const addNewBook=()=>{
+        const newBook = books.filter(item => !item.toLowerCase().includes('2'));
+        setBooks(newBook);
+    }
+
+    const memoizedBooks = useCallback(() => {
+        addNewBook()
+    }, [books]);
+
+    return <>
+        <button onClick={() => setCounter(counter + 1)}>+</button>
+        {counter}
+        <Book addBook={memoizedBooks}/>
+    </>;
+};
+
+
+
