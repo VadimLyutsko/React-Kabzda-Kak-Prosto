deaf595480941d3672565902573ff5e606ba8e1c
vadim <vadim_lyutsko@mail.ru>
2022-12-21 20:31:36 +0300

    Add Clock

--- stories/useEffect.stories.tsx
+++ stories/useEffect.stories.tsx
@@ -12,28 +12,70 @@ export const SimpleExample = () => {
 
     console.log('SimpleExample');
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect every render');
-        document.title = counter.toString()
-    })
+        document.title = counter.toString();
+    });
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect only first render (as componentDidMount)');
-        document.title = counter.toString()
-    })
+        document.title = counter.toString();
+    });
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log('useEffect only first render and every counter changes');
-        document.title = counter.toString()
-    },[counter])
+        document.title = counter.toString();
+    }, [counter]);
 
 
     return <>
-        Hello  {counter} {fake}
-        <button onClick={() => setCounter(counter+1)}>Counter+</button>
-        <button onClick={() => setFake(fake+1)}>fake+</button>
+        Hello {counter} {fake}
+        <button onClick={() => setCounter(counter + 1)}>Counter+</button>
+        <button onClick={() => setFake(fake + 1)}>fake+</button>
     </>;
 };
+
+export const SetTimeOutSimpleExample = () => {
+    const [counter, setCounter] = useState(1);
+
+    console.log('setTimeOutExample');
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1);
+        }, 1000);
+    }, []);
+
+    return <>
+        Hello Counter:{counter}
+    </>;
+};
+
+
+export const Clock = () => {
+
+    const date = new Date;
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(+date.getSeconds());
+    useEffect(() => {
+        setHours(+date.getHours());
+        setMinutes(+date.getMinutes());
+        setTimeout(() => {
+            if (seconds < 60) {
+                setSeconds(state => state + 1);
+            } else {
+                setSeconds(1);
+            }
+        }, 1000);
+    }, [seconds]);
+
+
+    return <>
+        Hours: {hours} Minutes:{minutes} Seconds:{seconds}
+    </>;
+};
+
 
 
 
