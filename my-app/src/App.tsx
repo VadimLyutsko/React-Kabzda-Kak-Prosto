efcc9c327b70b345b3be9f991dfa526d65d59555
vadim <vadim_lyutsko@mail.ru>
2023-01-13 13:30:40 +0300

    Added homepage

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,6 +6,7 @@ import {UnControlledRating} from './Components/Rating/UnControlledRating';
 import Accordion from './Components/Accordion/Accordion';
 import {UncontrolledOnnOff} from './Components/OnnOff/UncontrolledOnnOff';
 import {ControlledOnnOff} from './Components/OnnOff/ControlledOnnOff';
+import {Clock} from './Components/Clock/Clock';
 
 
 type PageTitlePropsType = {
@@ -22,23 +23,25 @@ function App() {
         <div className="App">
             {/*<Accordion titleValue={'Accordion-title # 1'} collapsed={true}/>*/}
             {/*<Accordion titleValue={titleValue} collapsed={accordionIsCollapsed} callBack={()=>setAccordionIsCollapsed(!accordionIsCollapsed)}/>*/}
-            {/*<UnControlledAccordion titleValue={'Accordion-title # 2'}/>*/}
+            <UnControlledAccordion titleValue={'Accordion-title # 2'}/>
 
-            {/*<UnControlledRating value={0}/>*/}
+            <UnControlledRating value={0}/>
 
 
-            {/*<Rating ratingValue={ratingValue} callback = {setRatingValue}/>*/}
-            {/*<UnControlledRating value={5}/>*/}
+            <Rating ratingValue={ratingValue} callback = {setRatingValue}/>
+            <UnControlledRating value={5}/>
             {/*<Rating value={1}/>*/}
             {/*<Rating value={2}/>*/}
             {/*<Rating value={3}/>*/}
             {/*<Rating value={4}/>*/}
             {/*<Rating value={5}/>*/}
 
-            {/*<UncontrolledOnnOff indicator={true}/>*/}
+            <UncontrolledOnnOff indicator={true}/>
             <ControlledOnnOffMemo indicator={IsButtonControlled} callBack={setIsButtonControlled}/>
-            {/*<UncontrolledOnnOff indicator={false}/>*/}
-            {/*<UncontrolledOnnOff indicator={false}/>*/}
+            <UncontrolledOnnOff indicator={false}/>
+            <UncontrolledOnnOff indicator={false}/>
+
+            <Clock/>
 
         </div>
     );
